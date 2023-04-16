df9b04f3197b4b9a192ee17da42a9b9be91f1338
Elizaveta Guseva <liza.guseva@hey.com>
2023-04-16 14:34:30 +0200

    pretends to submit

--- src/labyrinth_of_dreams/journal/ui/views/writer.cljs
+++ src/labyrinth_of_dreams/journal/ui/views/writer.cljs
@@ -1,4 +1,31 @@
-(ns labyrinth-of-dreams.journal.ui.views.writer)
+(ns labyrinth-of-dreams.journal.ui.views.writer
+  (:require [reagent.core :as r]
+            [labyrinth-of-dreams.journal.state :refer [app]]
+            [labyrinth-of-dreams.journal.ui.common :refer [button]]))
+
+(defn update-data [data key]
+  (fn [e]
+    (swap! data assoc key (.. e -target -value))))
+
+(defn textarea [data key label]
+  (let [id (str "field-" (name key))]
+    [:div.field
+     [:div.label
+      [:label {:for id} label]]
+     [:div.control
+  [:textarea.main-textarea 
+   {:id id
+    :on-change (update-data data key)
+    :value (get @data key "")
+    :placeholder "Here..."}
+   ]
+      ]]))
+
+(defn submit-button [data text]
+  (button text {:dispatch [:notes/create @data]} "submit-entry"))
 
 (defn writer []
-  [:textarea.main-textarea {:placeholder "Here..."}])
+  (let [form-data (r/cursor app [:entry-form])]
+     [:form.entry-form
+      (textarea form-data :content "")
+      (submit-button form-data "Save")]))
