556b295dff44ee7d481c6c01dd74a67d43bf35dd
Elizaveta Guseva <liza.guseva@hey.com>
2023-04-17 22:03:06 +0200

    saving notes with make-believe api

--- src/labyrinth_of_dreams/journal/ui/common.cljs
+++ src/labyrinth_of_dreams/journal/ui/common.cljs
@@ -2,26 +2,26 @@
   (:require [labyrinth-of-dreams.journal.command :refer [dispatch!]]))
 
 (defn handle-navigate [route-params]
-  (fn [_]
+  (fn [event]
+    (event/prevent-default event)
     (dispatch! :route/navigate route-params)))
 
 (defn handle-dispatch [command-data]
-  (fn [e]
-    (.preventDefault e)
+  (fn [evt]
+    (.preventDefault evt)
     (apply dispatch! command-data)))
 
 (defn button [text & {:keys [route-params dispatch on-click class]
                     :or {class ""
                          route-params nil
                          dispatch nil
-                         on-click identity
+                         on-click nil
                          }}]
   [:button
    {:class (str "button " class)
-    :on-click (
-               (cond
+    :on-click (cond
                 route-params (handle-navigate route-params)
                 dispatch (handle-dispatch dispatch)
                 on-click on-click
-                :else #(js/console.error "No action provided for button")))}
+                :else #(js/console.error "No action provided for button"))}
    text])
