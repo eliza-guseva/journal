1a9192fe605551ae5c78cd08792a6dada9b1e892
Elizaveta Guseva <liza.guseva@hey.com>
2023-04-26 15:30:01 +0200

    in the process of figuring out do-request

--- src/labyrinth_of_dreams/journal/api.cljs
+++ src/labyrinth_of_dreams/journal/api.cljs
@@ -1,4 +1,53 @@
-(ns labyrinth-of-dreams.journal.api)
+(ns labyrinth-of-dreams.journal.api
+  (:require [learn-cljs.notes.events :refer [emit!]]
+            [learn-cljs.notes.errors :as err]
+            [camel-snake-kebab.core :as csk]
+[camel-snake-kebab.extras :as cske]))
+
+(defn- display-error [err]
+  (emit! :notification/added
+         {:type :error
+          :text (str "API Error: " (ex-message err))}))
+
+(defn- with-error-handling [f]
+  (fn [res]
+    (->> res
+         (err/map f)
+         (err/unwrap-or display-error))))
+
+(defn do-request!
+  ([method path cb] (do-request! method path nil cb))
+  ([method path body cb]
+   (let [serialized-body (when body
+                           (->> body
+                                (cske/transform-keys csk/->camelCaseString)
+                                (clj->js)
+                                (js/JSON.stringify)))]
+     (-> (js/fetch (str js/API_URL path)
+                   (cond-> {:method (name method)
+                            :headers {"Authorization" (str "Bearer " js/API_TOKEN)}
+                            :credentials "include"}
+                     (some? body)
+                     (->
+                      (assoc :body serialized-body)
+                      (update :headers merge {"content-type" "application/json"}))
+
+                     :always
+                     clj->js))
+         (.then (fn [res]
+                  (if (.-ok res)
+                    (when (= 200 (.-status res))
+                      (.json res))
+                    (throw (ex-info "API Request Failed"
+                                    {:status-code (.-status res)
+                                     :status (.-statusText res)}
+                                    :api-failure)))))
+         (.then #(->> %
+                      (js->clj)
+                      (cske/transform-keys csk/->kebab-case-keyword)
+                      (err/ok)
+                      (cb)))
+         (.catch #(cb (err/error %)))))))
 
 (defn create-note! [note]
   (js/console.log 
@@ -10,3 +59,7 @@
     (js/console.log 
      (str "make believe updating note!"
             (pr-str note))))
+
+(defn get-notes! []
+  (do-request! :get "/notes"
+               (with-error-handling #(emit! :notes/received %))))
